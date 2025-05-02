703bee5e2bcb4153aa327a8fa5d3d4cac35e9ece
WaruCodes <sdrwarushika@std.appsc.sab.ac.lk>
2025-05-02 22:14:39 +0530

    Next Commit

--- eBook-App/src/main/java/com/DAO/UserDAOimpl.java
+++ eBook-App/src/main/java/com/DAO/UserDAOimpl.java
@@ -18,12 +18,18 @@ public class UserDAOimpl implements UserDAO{
 	public boolean userRegister(User us) {
 		boolean f=false;
 		try {
-			String sql="insert into user(name, email, phno, password) values(";
+			String sql="insert into user(name, email, phno, password) values(?,?,?,?)";
 			PreparedStatement ps=conn.prepareStatement(sql);
 			ps.setString(1, us.getName());
 			ps.setString(2, us.getEmail());
 			ps.setString(3, us.getPhno());
 			ps.setString(4, us.getPassword()); 
+			
+			int i=ps.executeUpdate();
+			if(i==1) {
+				f=true;
+			}
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
